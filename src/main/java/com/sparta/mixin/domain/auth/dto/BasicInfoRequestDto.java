15ef29f9075176decf33a48af9eb5d339cc96b63
haejungHan <tinley705@gmail.com>
2024-09-03 23:51:18 +0900

    add : 회원가입 기능 기초세팅

---
+++ src/main/java/com/sparta/mixin/domain/auth/dto/BasicInfoRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.mixin.domain.auth.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BasicInfoRequestDto {
+
+	private String userName;
+	private String gender;
+	private String phoneNumber;
+}
---
+++ src/main/java/com/sparta/mixin/domain/auth/dto/EmailVerifyRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.mixin.domain.auth.dto;
+
+import lombok.Getter;
+
+@Getter
+public class EmailVerifyRequestDto {
+
+	private String email;
+}
