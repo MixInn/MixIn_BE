27287c0d825b9222055f95dfeb43a3e1988a7672
hongsy521 <seoyeong0521@naver.com>
2024-09-23 23:50:37 +0900

    feat : 커뮤니티 좋아요 기능 추가

--- src/main/java/com/sparta/mixin/domain/community/like/entity/CommunityLike.java
+++ src/main/java/com/sparta/mixin/domain/community/like/entity/CommunityLike.java
@@ -2,20 +2,29 @@ package com.sparta.mixin.domain.community.like.entity;
 
 import com.sparta.mixin.domain.community.CommunityType;
 import com.sparta.mixin.domain.community.meetpost.entity.MeetPost;
+import com.sparta.mixin.domain.community.publicpost.entity.PublicPost;
 import com.sparta.mixin.domain.user.entity.User;
 import com.sparta.mixin.global.Timestamped;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Entity
-@Table(name = "communityLike")
+@Getter
+@Table(name = "community_like")
+@RequiredArgsConstructor
 public class CommunityLike extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "community_id")
-    private MeetPost community;
+    @JoinColumn(name = "publicPost_id")
+    private PublicPost publicPost;
+
+    @ManyToOne
+    @JoinColumn(name = "meetPost_id")
+    private MeetPost meetPost;
 
     @Enumerated(EnumType.STRING)
     private CommunityType communityType;
@@ -24,6 +33,12 @@ public class CommunityLike extends Timestamped {
     @JoinColumn(name = "user_id")
     private User user;
 
-
-    // Getters and setters
+    public CommunityLike(PublicPost publicPost) {
+        this.publicPost=publicPost;
+        this.communityType=CommunityType.PUBLICPOST;
+    }
+    public CommunityLike(MeetPost meetPost) {
+        this.meetPost=meetPost;
+        this.communityType=CommunityType.MEETPOST;
+    }
 }
