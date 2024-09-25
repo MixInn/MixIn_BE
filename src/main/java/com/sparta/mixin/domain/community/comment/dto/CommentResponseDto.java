38748e7b4acb84e1640af12a3971d51d21fc8497
hongsy521 <seoyeong0521@naver.com>
2024-09-25 17:15:29 +0900

    feat : comment 응답형식 수정

--- src/main/java/com/sparta/mixin/domain/community/comment/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/mixin/domain/community/comment/dto/CommentResponseDto.java
@@ -1,9 +1,12 @@
 package com.sparta.mixin.domain.community.comment.dto;
 
+import com.sparta.mixin.domain.community.CommunityType;
 import com.sparta.mixin.domain.community.comment.entity.CommunityComment;
 
 public class CommentResponseDto {
     private Long id;
+    private Long postId;
+    private CommunityType communityType;
     private String comment;
 
     public CommentResponseDto(CommunityComment communityComment) {
