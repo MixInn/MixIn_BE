aed0b55cdfdc4bb23ceee13161ea0a214d08de31
yoodab <yoondab2166@gmail.com>
2024-10-30 21:18:18 +0900

    feat : meet 수정/삭제 유저 확인 추가

--- src/main/java/com/sparta/mixin/domain/meet/controller/MeetController.java
+++ src/main/java/com/sparta/mixin/domain/meet/controller/MeetController.java
@@ -1,11 +1,13 @@
 package com.sparta.mixin.domain.meet.controller;
 
+import com.sparta.mixin.domain.auth.security.UserDetailsImpl;
 import com.sparta.mixin.domain.meet.dto.MeetRequestDto;
 import com.sparta.mixin.domain.meet.service.MeetService;
 import com.sparta.mixin.global.common.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,23 +17,25 @@ public class MeetController {
     private final MeetService meetService;
 
     @PostMapping
-    public ResponseEntity<CommonResponse> createMeet(@RequestBody MeetRequestDto requestDto) {
-        meetService.createMeet(requestDto);
+    public ResponseEntity<CommonResponse> createMeet(@RequestBody MeetRequestDto requestDto,@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        meetService.createMeet(requestDto,userDetails.getUser());
         CommonResponse response = new CommonResponse<>("모임 생성 성공", 201, "");
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/{meetId}")
-    public ResponseEntity<CommonResponse> updateMeet(@PathVariable(name = "meetId") Long meetId, @RequestBody MeetRequestDto requestDto) {
-        meetService.updateMeet(meetId, requestDto);
+    public ResponseEntity<CommonResponse> updateMeet(@PathVariable(name = "meetId") Long meetId, @RequestBody MeetRequestDto requestDto,
+                                                     @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        meetService.updateMeet(meetId, requestDto,userDetails.getUser());
         CommonResponse response = new CommonResponse<>("모임 수정 성공", 201, "");
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
 
     @DeleteMapping("/{meetId}")
-    public ResponseEntity<CommonResponse> deleteMeet(@PathVariable(name = "meetId") Long meetId) {
-        meetService.deleteMeet(meetId);
+    public ResponseEntity<CommonResponse> deleteMeet(@PathVariable(name = "meetId") Long meetId,
+                                                     @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        meetService.deleteMeet(meetId,userDetails.getUser());
         CommonResponse response = new CommonResponse<>("모임 삭제 성공", 201, "");
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
