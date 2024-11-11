68b5bb637e989cc7d498591946cfecf4fed08ee1
hongsy521 <seoyeong0521@naver.com>
2024-11-12 01:01:32 +0900

    feat : 글 단건조회, 글 수정, 글 삭제 공통기능 PostController로 묶어서 url 경로 통일하기

--- src/main/java/com/sparta/mixin/domain/post/meetpost/MeetPostController.java
+++ src/main/java/com/sparta/mixin/domain/post/meetpost/MeetPostController.java
@@ -54,46 +54,6 @@ public class MeetPostController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/{postId}")
-    public ResponseEntity<CommonResponse<PostResponseDto>> getMeetPost(
-        @PathVariable(name = "postId") Long postId, @AuthenticationPrincipal
-    UserDetailsImpl userDetails) {
-        PostResponseDto responseDto = meetPostService.getPost(postId, userDetails.getUser());
-        CommonResponse response = new CommonResponse<>("밋커뮤니티 단건 글 조회 성공", 200, responseDto);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @PutMapping("/{postId}")
-    public ResponseEntity<CommonResponse<PostResponseDto>> editMeetPost(
-        @PathVariable(name = "postId") Long postId,
-        @RequestPart("requestDto") PostRequestDto postRequestDto,
-        @RequestPart(value = "files", required = false) List<MultipartFile> files,
-        @AuthenticationPrincipal
-        UserDetailsImpl userDetails) {
-        List<String> fileUrls = new ArrayList<>();
-
-        for (MultipartFile file : files) {
-            imageService.validateFile(file);
-            String fileUrl = imageService.getFileUrl(file);
-            fileUrls.add(fileUrl);
-        }
-
-        PostResponseDto responseDto = meetPostService.editPost(postId, postRequestDto, fileUrls,
-            userDetails.getUser());
-        CommonResponse response = new CommonResponse<>("밋커뮤니티 글 수정 성공", 200, responseDto);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{postId}")
-    public ResponseEntity<CommonResponse> deleteMeetPost(
-        @PathVariable(name = "postId") Long postId,
-        @AuthenticationPrincipal
-        UserDetailsImpl userDetails) {
-        meetPostService.deletePost(postId, userDetails.getUser());
-        CommonResponse response = new CommonResponse("밋커뮤니티 글 삭제 성공", 204, "");
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
     @GetMapping("/{meetId}/all")
     public ResponseEntity<CommonResponse<Page<PostResponseDto>>> getAllMeetPost(
         @PathVariable(name = "meetId") Long meetId,
